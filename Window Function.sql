5794d94747c2a6f6ce2de85cb69dc66631606241
Naina Johri <johrinaina80@gmail.com>
2025-07-07 23:22:44 +0530

    Add files via upload

---
+++ Window Function.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE IF NOT EXISTS windowdb;
+use windowdb;
+CREATE TABLE DATA(
+new_id int,
+new_cat VARCHAR(20)
+);
+
+INSERT INTO DATA VALUES 
+(100, 'Agni'),
+(200, 'Agni'),
+(500, 'Dharti'),
+(700, 'Dharti'),
+(200, 'Vayu'),
+(300, 'Vayu'),
+(500, 'Vayu');
+
+
+-- 	Aggregate  window fun
+
+-- Partition By
+SELECT new_id, new_cat,
+SUM(new_id) OVER(PARTITION BY new_cat) AS "Total",
+AVG(new_id) OVER(PARTITION BY new_cat) AS "Average",
+COUNT(new_id) OVER(PARTITION BY new_cat) AS "Count",
+MIN(new_id) OVER(PARTITION BY new_cat) AS "Min",
+MAX(new_id) OVER(PARTITION BY new_cat) AS "Max"
+FROM DATA;
+
+-- Rows
+SELECT new_id, new_cat,
+SUM(new_id) OVER(ORDER BY new_id ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS "Total",
+AVG(new_id) OVER(ORDER BY new_id ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS "Average",
+COUNT(new_id) OVER(ORDER BY new_id ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS "Count",
+MIN(new_id) OVER(ORDER BY new_id ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS "Min",
+MAX(new_id) OVER(ORDER BY new_id ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS "Max"
+FROM DATA;
+
+-- Rank
+SELECT new_id,
+ROW_NUMBER() OVER(ORDER BY new_id) AS "Row_Number",
+Rank() OVER(ORDER BY new_id) AS "Rank",
+DENSE_RANK() OVER(ORDER BY new_id) AS "Dense_Rank",
+PERCENT_RANK() OVER(ORDER BY new_id) AS "Percent_Rank"
+FROM DATA;
+
+-- Analytic function
+-- Ifwe dont use any Partition BY or ROWS Clause then in case of LAST_VALUE we will get same col value 
+-- If we actually want the LAST_VALUE We should use ROWS BETWWEN/ UNBOUNEDE PRECEDING/ UNBOUBDED FOLLOWING
+SELECT new_id,
+FIRST_VALUE(new_id) OVER(ORDER BY new_id) AS "FIRST VALUE",
+LAST_VALUE(new_id) OVER(ORDER BY new_id) AS "LAST VALUE",
+LEAD(new_id) OVER(ORDER BY new_id) AS "LEAD",
+LAG(new_id) OVER(ORDER BY new_id) AS "LAG"
+FROM DATA;
